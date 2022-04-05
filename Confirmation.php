da3f0ffac7358f1c18aefd5bfae50db9bb245f13
Tyronne282 <bradburntyronne@gmail.com>
2022-04-05 19:10:26 +0100

    Completed Logbook 4

--- Confirmation.php
+++ Confirmation.php
@@ -1,132 +1,32 @@
 <?php
+   echo "<h2> Your order qty is $_POST[hdqty] </h2></br>";
+   echo "<h2> and the selected colour is $_POST[selcolour].</h2></br>";
 
-  //Hidden field
-  /*
-  $qty = $_POST['selqty'];
-  $size = $_POST['selsize'];
-  $colour = $_POST['selcolour'];
-  switch ($size) {
-    case 'Small':
-      $total = $qty * 15.75;
-      break;
-    case 'Medium':
-      $total = $qty * 16.75;
-      break;
-    case 'Large':
-      $total = $qty * 17.75;
-      break;
-    case 'Extra Large':
-      $total = $qty * 18.75;
-      break;
-    default:
-      $total = "Undefined";
-      break;
-  }
-  if(isset($_POST['selqty']))
-  {
-    echo "<h2> Your order qty is " . intval($qty) . ".</h2>";
-  }
-  if(isset($_POST['selsize']))
-  {
-    echo "<h2> The size of the ordered widgets are $size.</h2>";
-  }
-  if(isset($_POST['selcolour']))
-  {
-    echo "<h2> The selected colour is $colour.</h2>";
-  }
-  if(isset($total) && $total != "Undefined")
-  {
-    echo "<h2><hr/> The total price of your purchase is: £" . $total . "</h2>";
-  }
-  */
+   if($_POST) 
+   {
+      $qty = $_POST["hdqty"];
+     $price = $_POST["trueprice"];
 
-  //Cookie
-  /*
-  $qty = $_COOKIE['selqty'];
-  $size = $_COOKIE['selsize'];
-  $colour = $_POST['selcolour'];
-  switch ($size) {
-    case 'Small':
-      $total = $qty * 15.75;
-      break;
-    case 'Medium':
-      $total = $qty * 16.75;
-      break;
-    case 'Large':
-      $total = $qty * 17.75;
-      break;
-    case 'Extra Large':
-      $total = $qty * 18.75;
-      break;
-    default:
-      $total = "Undefined";
-      break;
-  }
-  if(isset($_COOKIE['selqty']))
-  {
-    echo "<h2> Your order qty is " . intval($qty) . ".</h2>";
-  }
-  if(isset($_COOKIE['selsize']))
-  {
-    echo "<h2> The size of the ordered widgets are $size.</h2>";
-  }
-  if(isset($_POST['selcolour']))
-  {
-    echo "<h2> The selected colour is $colour.</h2>";
-  }
-  if(isset($total) && $total != "Undefined")
-  {
-    echo "<h2><hr/> The total price of your purchase is: £" . $total . "</h2>";
-  }
-  */
-
-  //Session
-  session_start();
-
-  $qty = $_SESSION['selqty'];
-  $colour = $_POST['selcolour'];
-  $size = $_SESSION['selsize'];
-
-  switch ($size) {
-
-    case 'Small':
-
-      $total = $qty * 15.75;
-      break;
-
-    case 'Medium':
-      $total = $qty * 16.75;
-      break;
-
-    case 'Large':
-      $total = $qty * 17.75;
-      break;
-
-    case 'Extra Large':
-      $total = $qty * 18.75;
-      break;
-
-    default:
-      $total = "Undefined";
-      break;
-  }
-
-  if(isset($_SESSION['selqty']))
-  {
-    echo "<h2> Number of widgets: " . intval($qty) . "</h2>";
-  }
-
-  if(isset($_SESSION['selsize']))
-  {
-    echo "<h2> Size of widgets: $size </h2>";
-  }
-
-  if(isset($_POST['selcolour']))
-  {
-    echo "<h2> Selected colour: $colour</h2>";
-  }
-  if(isset($total) && $total != "Undefined")
-  {
-    echo "<h2><hr/> Total price: £" . $total . "</h2>";
+     if($_POST["trueprice"] == "Small")
+     {
+       $total =  $qty * 15.75;
+     }
+     else if($_POST["trueprice"] == "Medium")
+     {
+       $total =  $qty * 16.75;
+     }
+     else if($_POST["trueprice"] == "Large")
+     {
+       $total =  $qty * 17.75;
+     }
+     else if($_POST["trueprice"] == "Extra Large")
+     {
+       $total =  $qty * 17.75;
+     }
+     else 
+     {
+        echo "You have not included a price to your widgets";
+     }    
+      echo("<h2>Total price of widget is: $total </h2>");       
   }
 ?>
--- select colour.php
+++ select colour.php
@@ -1,36 +1,21 @@
-<?php
-
-  //Cookie
-  /*
-  setcookie('selqty', $_POST['selqty']);
-  */
-
-  //Session
-
-  session_start();
-  $_SESSION['selsize'] = $_POST['selsize'];
-
-?>
-
 <html>
   <head><title>Select colour page</title></head>
     <body>
       <form action="confirmation.php"  method="post">
-	      Select the colour for the <?php echo intval($_SESSION['selqty']) ?> widgets you are ordering
-       	<select name="selcolour">
-       	  <option>white</option>
-      	  <option>red</option>
-      	  <option>yellow</option>
-      	  <option>green</option>
-      	  <option>blue</option>
+	Select the colour for the
+  <input type="hidden" name = "trueprice" value = "<?php echo $_POST["Size"] ?>"/>
+  <input type="hidden" name="hdqty" value="<?php echo $_POST['selqty'] ?>"/>
+  widgets you are ordering
+   	<select name="selcolour">
+   	  <option>white</option>
+	  <option>red</option>
+	  <option>yellow</option>
+	  <option>green</option>
+	  <option>blue</option>
         </select>
-        <br/><br/>
-        <!-- The hidden field -->
-        <!--
-        <input type="hidden" name="selqty" value="<?php //echo $_POST['selqty']?>"/>
-        <input type="hidden" name="selsize" value="<?php //echo $_POST['selsize']?>"/>
-        -->
+        <br/><br/>	
         <input type="submit" value="Buy"/>
+
       </form>
    </body>
-</html>
+</html>	
