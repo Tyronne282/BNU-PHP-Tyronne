75697245cd436c574016213d3f4071e8e48d5632
Tyronne282 <bradburntyronne@gmail.com>
2022-04-06 11:47:16 +0100

    updated

--- select colour.php
+++ select colour.php
@@ -2,20 +2,19 @@
   <head><title>Select colour page</title></head>
     <body>
       <form action="confirmation.php"  method="post">
-	Select the colour for the
-  <input type="hidden" name = "trueprice" value = "<?php echo $_POST["Size"] ?>"/>
-  <input type="hidden" name="hdqty" value="<?php echo $_POST['selqty'] ?>"/>
-  widgets you are ordering
+	      Select the colour for the
+        <input type="hidden" name = "trueprice" value = "<?php echo $_POST["Size"] ?>"/>
+        <input type="hidden" name="hdqty" value="<?php echo $_POST['selqty'] ?>"/>
+        widgets you are ordering
    	<select name="selcolour">
    	  <option>white</option>
-	  <option>red</option>
-	  <option>yellow</option>
-	  <option>green</option>
-	  <option>blue</option>
+	    <option>red</option>
+	    <option>yellow</option>
+	    <option>green</option>
+	    <option>blue</option>
         </select>
         <br/><br/>	
         <input type="submit" value="Buy"/>
-
       </form>
    </body>
 </html>	
